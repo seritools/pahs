c1c60c6f86c01b3697eaf68656f3547e39495abb
Dennis Duda <git@seri.tools>
2021-01-10 01:11:18 +0100

    feat(push): add default trait impls for HashMap

--- src/push.rs
+++ src/push.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::hash::{BuildHasher, Hash};
+
 /// Helper trait for collections to push into.
 pub trait Push<T> {
     /// Pushes a value into the collection.
@@ -22,4 +25,26 @@ impl<T> Push<T> for () {
     /// Discards the value.
     #[inline]
     fn push(&mut self, _value: T) {}
+}
+
+impl<K, V, S> Push<(K, V)> for HashMap<K, V, S>
+where
+    K: Eq + Hash,
+    S: BuildHasher,
+{
+    #[inline]
+    fn push(&mut self, (key, value): (K, V)) {
+        self.insert(key, value);
+    }
+}
+
+impl<K, V, S> Push<(K, V)> for &mut HashMap<K, V, S>
+where
+    K: Eq + Hash,
+    S: BuildHasher,
+{
+    #[inline]
+    fn push(&mut self, (key, value): (K, V)) {
+        self.insert(key, value);
+    }
 }
